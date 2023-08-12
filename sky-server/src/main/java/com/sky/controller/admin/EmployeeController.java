2bc719aa69f25cbcf20b6dd0bdf60fa167fcc0db
JianYu <xiaonzhan@outlook.com>
2023-08-12 15:41:20 +0800

    员工管理模块-实现 员工分页查询 功能

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -3,8 +3,10 @@ package com.sky.controller.admin;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
@@ -13,13 +15,12 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.sky.result.Result.success;
 
 /**
  * 员工管理
@@ -63,7 +64,7 @@ public class EmployeeController {
                 .token(token)
                 .build();
 
-        return Result.success(employeeLoginVO);  //使用Result格式再次封装
+        return success(employeeLoginVO);  //使用Result格式再次封装
     }
 
     /**
@@ -74,25 +75,41 @@ public class EmployeeController {
     @ApiOperation(value = "员工退出")
     @PostMapping("/logout")
     public Result<String> logout() {
-        return Result.success();
+        return success();
     }
 
     /**
      * 新增员工
+     *
      * @param employeeDTO
      * @return
      */
     @ApiOperation("新增员工")
     @PostMapping
-    public Result save(@RequestBody EmployeeDTO employeeDTO){
+    public Result save(@RequestBody EmployeeDTO employeeDTO) {
 
         log.info("新增员工：{}", employeeDTO);
 
         //调用业务层处理
         employeeService.save(employeeDTO);
         //返回成功结果
-        return Result.success();
+        return success();
     }
 
+    /**
+     * 员工分页查询
+     *
+     * @param employeePageQueryDTO
+     * @return
+     */
+    @ApiOperation("分页查询")
+    @GetMapping("/page")
+    public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO) {
+        log.info("员工分页查询，参数为：", employeePageQueryDTO);
+        //调用业务层查询
+        PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
+        //返回封装结果
+        return Result.success(pageResult);
+    }
 
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,16 +1,22 @@
 package com.sky.service.impl;
 
+
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
+import com.sky.result.PageResult;
 import com.sky.service.EmployeeService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -90,5 +96,20 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //调用持久层插入数据
         employeeMapper.insert(employee);
+    }
+
+    /**
+     * 分页查询员工
+     * @param employeePageQueryDTO
+     * @return
+     */
+    @Override
+    public PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO) {
+        //使用PageHelper插件开启分页查询
+        PageHelper.startPage(employeePageQueryDTO.getPage(), employeePageQueryDTO.getPageSize());
+        //调用持久层查询
+        Page<Employee> page = employeeMapper.pageQuery(employeePageQueryDTO);
+        //封装查询结果为PageResult并返回
+        return new PageResult(page.getTotal(), page.getResult());
     }
 }
