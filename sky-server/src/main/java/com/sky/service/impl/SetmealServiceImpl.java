4340901d5e9f7f110da090551acf8eea3312df24
JianYu <xiaonzhan@outlook.com>
2023-08-18 16:25:02 +0800

    实现套餐管理模块 套餐分页查询 功能

--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -1,11 +1,17 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +28,7 @@ public class SetmealServiceImpl implements SetmealService {
     private SetmealMapper setmealMapper;
     @Autowired
     private SetmealDishMapper setmealDishMapper;
+
 
     /**
      * 新增菜品
@@ -43,5 +50,20 @@ public class SetmealServiceImpl implements SetmealService {
         });
         //将套餐所包含的菜品信息存入套餐菜品关系表
         setmealDishMapper.insertBatch(setmealDishes);
+    }
+
+    /**
+     * 套餐分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @Override
+    public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
+        //开启分页
+        PageHelper.startPage(setmealPageQueryDTO.getPage(), setmealPageQueryDTO.getPageSize());
+        //分页查询
+        Page<SetmealVO> page = setmealMapper.pageQuery(setmealPageQueryDTO);
+        log.debug("菜品分页查询结果：{}", page.getResult());
+        return new PageResult(page.getTotal(), page.getResult());
     }
 }
